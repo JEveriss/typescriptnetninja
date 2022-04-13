f0b282ddb24a7d4d0f524e88ddbf3460fdb2da01
Jacques Everiss <jacques.everiss@gmail.com>
2022-04-13 17:32:47 +0100

    cors error solved, liveserver installed

--- public/app.js
+++ public/app.js
@@ -1,35 +1,4 @@
-"use strict";
-// const anchor = document.querySelector("a")!;
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-// console.log(anchor.href);
-// const form = document.querySelector("form")!;
-// console.log(form);
-// classes
-class Invoice {
-    //   readonly client: string;
-    //   private details: string;
-    //   public amount: number;
-    //   // these are 'access modifiers' that allow certain access to the information.
-    //   // Public is default - can be seen and changed
-    //   // Readonly - can be seen but not changed
-    //   // Private - neither
-    //   constructor(c: string, d: string, a: number) {
-    //     this.client = c;
-    //     this.details = d;
-    //     this.amount = a;
-    //   }
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    // this shorter way of writing only works with the access modifiers
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/invoices.js";
 const invOne = new Invoice("Mario", "plumbing work", 69);
 const invTwo = new Invoice("Luigi", "fixing me pipes", 420);
 let invoices = [];
